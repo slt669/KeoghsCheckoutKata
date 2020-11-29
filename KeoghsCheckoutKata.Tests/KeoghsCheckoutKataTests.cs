f1aec2472676abfe2e270acfdc698e55ab32336e
Stephen Thompson <slthompson669@hotmail.co.uk>
2020-11-29 15:50:30 +0000

    Set up all tests, input data and mock repository

--- KeoghsCheckoutKata.Tests/KeoghsCheckoutKataTests.cs
+++ KeoghsCheckoutKata.Tests/KeoghsCheckoutKataTests.cs
@@ -53,6 +53,131 @@ namespace KeoghsCheckoutKata.Tests
 
 
         }
-       
+        /// <summary>
+        /// Given items have been added to the basket Then the total cost of the basket should be calculated
+        /// </summary>
+        /// <param name="scan"></param>
+        /// <param name="expected"></param>
+        [Theory]
+        [InlineData("A", 10)]
+        [InlineData("B", 15)]
+        [InlineData("C", 40)]
+        [InlineData("D", 55)]
+        [InlineData("AB", 25)]
+        [InlineData("AC", 50)]
+        [InlineData("AAAA", 40)]
+        [InlineData("CCCC", 160)]
+        [InlineData("ACAC", 100)]
+        public void Given_items_added_Then_total_of_basket_should_be_calculated(string items, int expected)
+        {
+            //Arrange
+            //Act
+            //Assert
+
+            var listOfProducts = new List<Product>();
+
+            listOfProducts.Add(
+            new Product { SKU = 'A', Price = 10 });
+            listOfProducts.Add(
+            new Product { SKU = 'B', Price = 15 });
+            listOfProducts.Add(
+            new Product { SKU = 'C', Price = 40 });
+            listOfProducts.Add(
+            new Product { SKU = 'D', Price = 55 });
+
+            var listOfDiscounts = new List<Discount>();
+
+            listOfDiscounts.Add(
+            new Discount { SKU = 'B', Quantity = 3, Value = 5 });
+            listOfDiscounts.Add(
+            new Discount { SKU = 'D', Quantity = 2, Value = 27.5m });
+
+            // Mock Allows me to test an isolated Repository  
+
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(x => x.GetProducts()).Returns(listOfProducts);
+            mockRepository.Setup(x => x.GetDiscounts()).Returns(listOfDiscounts);
+        }
+        /// <summary>
+        /// Given I have added a multiple of 3 lots of item ‘B’ to the basket Then a promotion of ‘3 for 40’ should be applied to every multiple of 3 (see: Grid 1).
+        /// </summary>
+        /// <param name="scan"></param>
+        /// <param name="expected"></param>
+        [Theory]
+        [InlineData("B", 15)]
+        [InlineData("BB", 30)]
+        [InlineData("BBB", 40)]
+        [InlineData("BBBB", 55)]
+        public void Given_added_multiple_of_3_of_SKU_B_3_for_40(string items, int expected)
+        {
+            //Arrange
+            //Act
+            //Assert
+
+            var listOfProducts = new List<Product>();
+
+            listOfProducts.Add(
+            new Product { SKU = 'A', Price = 10 });
+            listOfProducts.Add(
+            new Product { SKU = 'B', Price = 15 });
+            listOfProducts.Add(
+            new Product { SKU = 'C', Price = 40 });
+            listOfProducts.Add(
+            new Product { SKU = 'D', Price = 55 });
+
+            var listOfDiscounts = new List<Discount>();
+
+            listOfDiscounts.Add(
+            new Discount { SKU = 'B', Quantity = 3, Value = 5 });
+            listOfDiscounts.Add(
+            new Discount { SKU = 'D', Quantity = 2, Value = 27.5m });
+
+            // Mock Allows me to test an isolated Repository  
+
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(x => x.GetProducts()).Returns(listOfProducts);
+            mockRepository.Setup(x => x.GetDiscounts()).Returns(listOfDiscounts);
+        }
+        /// <summary>
+        /// Given I have added a multiple of 2 lots of item ‘D’ to the basket Then a promotion of ‘25% off’ should be applied to every multiple of 2 (see: Grid 1).
+        /// </summary>
+        /// <param name="scan"></param>
+        /// <param name="expected"></param>
+        [Theory]
+        [InlineData("D", 55)]
+        [InlineData("DD", 82.5)]
+        [InlineData("DDD", 137.5)]
+        [InlineData("DDDD", 165)]
+        public void Given_added_multiple_of_2_of_SKU_D_25_percent_off(string items, decimal expected)
+        {
+            //Arrange
+            //Act
+            //Assert
+
+            var listOfProducts = new List<Product>();
+
+            listOfProducts.Add(
+            new Product { SKU = 'A', Price = 10 });
+            listOfProducts.Add(
+            new Product { SKU = 'B', Price = 15 });
+            listOfProducts.Add(
+            new Product { SKU = 'C', Price = 40 });
+            listOfProducts.Add(
+            new Product { SKU = 'D', Price = 55 });
+
+            var listOfDiscounts = new List<Discount>();
+
+            listOfDiscounts.Add(
+            new Discount { SKU = 'B', Quantity = 3, Value = 5 });
+            listOfDiscounts.Add(
+            new Discount { SKU = 'D', Quantity = 2, Value = 27.5m });
+
+            // Mock Allows me to test an isolated Repository  
+
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(x => x.GetProducts()).Returns(listOfProducts);
+            mockRepository.Setup(x => x.GetDiscounts()).Returns(listOfDiscounts);
+
+        }
     }
 }
