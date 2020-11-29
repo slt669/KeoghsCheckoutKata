5a0e67b4c7c25ba2676726e2f4e853d98c6d8fce
Stephen Thompson <slthompson669@hotmail.co.uk>
2020-11-29 15:19:45 +0000

    Set up repository pattern (Product data in DB)

---
+++ KeoghsCheckoutKata.Library/Checkout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeoghsCheckoutKata.Library
+{
+    public class Checkout 
+    {
+        IRepository repository;
+
+        public Checkout(IRepository repository)
+        {
+            this.repository = repository;
+
+        }
+
+    }
+}
--- KeoghsCheckoutKata.Library/Repository.cs
+++ KeoghsCheckoutKata.Library/Repository.cs
@@ -2,7 +2,8 @@
 
 namespace KeoghsCheckoutKata.Library
 {
-    public class Class1
+    public class Repository : IRepository
     {
+
     }
 }
