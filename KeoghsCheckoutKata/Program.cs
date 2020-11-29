d663e25ff0a1e477275c836f185dae6036f7fa0b
Stephen Thompson <slthompson669@hotmail.co.uk>
2020-11-29 15:02:04 +0000

    Add project files.

---
+++ KeoghsCheckoutKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeoghsCheckoutKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
